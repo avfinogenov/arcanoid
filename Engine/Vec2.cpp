10cbce3e1e576ed629ac9963b966086ebbb1da3f
Андрей <@->
2017-08-24 15:02:07 +0300

    добавил вектор

---
+++ Engine/Vec2.cpp
@@ -0,0 +1,33 @@
+#include "Vec2.h"
+
+
+
+Vec2::Vec2()
+{
+}
+
+
+Vec2 Vec2::operator+(const Vec2 & rhs) const
+{
+	return Vec2(x+rhs.x, y+rhs.y);
+}
+
+Vec2 & Vec2::operator+=(const Vec2 & rhs)
+{
+	return *this = *this + rhs;
+
+}
+
+Vec2 Vec2::operator*(float rhs) const
+{
+	return Vec2(x*rhs, y*rhs);
+}
+
+Vec2 & Vec2::operator*=(float rhs)
+{
+	return *this = *this * rhs;
+}
+
+Vec2::~Vec2()
+{
+}
